3ee5ebd033f9ada0e8165b06878dda722cc97240
hazemabdelaal <hazemsabdelaal@gmail.com>
2021-08-16 15:35:52 +0200

    error fix

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -11,7 +11,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
       className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
     >
       <Image
-        layouts="responsive"
+        layout="responsive"
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
           `${BASE_URL}${result.poster_path}`
@@ -25,7 +25,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
           {result.title || result.original_name}
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {result.media_type && `${result.media_type} .`}{' '}
           {result.release_date || result.first_air_date}{' '}
           <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
         </p>
